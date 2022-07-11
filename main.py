1a1db297f90f625dda86443c86af7f0bd21a52ef
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2022-07-11 19:19:05 +0300

    Update main.py
    some messages

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from operator import index
+
+
 print("hello")
+question_list = ["How are you doing?"]
+answer_list = ["I am good"]
+
+query = input("You: ")
+if query in question_list:
+    print(answer_list[question_list.index(query)])
+
+else:
+    print("NO!!")
